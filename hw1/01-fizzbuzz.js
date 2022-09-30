5e28525183baa99dfabf6683d1ff6c687d22d9c2
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-09-30 07:57:41 +0530

    hw1 code for fizzbuzz

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,6 +11,18 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   // Add your code here
+  let output;
+  for (i = 1; i <= 100; i++) {
+    output = i;
+    if (i % 3 == 0 && i % 5 == 0) {
+      output = `fizzbuzz`;
+    } else if (i % 3 == 0) {
+      output = `fizz`;
+    } else if (i % 5 == 0) {
+      output = `buzz`;
+    }
+    console.log(output)
+  }
 };
 
 fizzbuzz();
