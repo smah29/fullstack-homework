4dab3ff5c18eaee999ef9334a89c9715ede9276c
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-10-13 13:18:03 +0530

    form commited

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,25 @@
 /** Exercise 03 - Form **/
 
-// Add your code here
+let form = document.querySelector('form');
+const NO_FEEDBACK = 'No feedback was submitted.';
+const NOT_SUBSCRIBED = 'No, thank you.';
+const SUBSCRIBED = 'Yes, I would like to join the newsletter.';
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    let message = form.elements.message.value;
+    message = message.length === 0 ? NO_FEEDBACK : message;
+    const signup = form.elements.signup.checked ? SUBSCRIBED : NOT_SUBSCRIBED
+
+    console.group('================ Form Submission ==================');
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Feedback: ' + message);
+    console.log('Newsletter: ' + signup);
+    console.groupEnd();
+
+    event.preventDefault();
+}
