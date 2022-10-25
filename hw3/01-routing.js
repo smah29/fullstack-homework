611b4a1d9e906332ad70798cfde598d44646d395
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-10-25 16:01:26 +0530

    01-routing.js commited

--- hw3/01-routing.js
+++ hw3/01-routing.js
@@ -44,6 +44,65 @@ const server = http.createServer((req, res) => {
   }
 
   // Add your code here
+  else if (req.url === '/welcome') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Welcome!!</h1>');
+    res.end();
+  }
+
+  else if (req.url === '/redirect') {
+    res.writeHead(302, { 'Location': '/redirected', 'Content-Type': 'text/html' });
+    res.end();
+  }
+
+  else if (req.url === '/redirected') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Welcome to redirected page</h1>');
+    res.end();
+  }
+
+  else if (req.url === '/cache') {
+    res.writeHead(200, { 'Content-Type': 'text/html', 'Cache-control': 'max-age= 86400' });
+    res.write('This resource was cached');
+    res.end();
+  }
+  else if (req.url === '/cookie') {
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Set-Cookie': 'hello=world' });
+    res.write('cookies... yummm');
+    res.end();
+  }
+
+  else if (req.url === '/check-cookies') {
+    let cookies = {};
+
+    req.headers &&
+      req.headers.cookie &&
+      req.headers.cookie.split(";").forEach(function (cookie) {
+        console.log("cookie is:", cookie);
+        if (cookie.match(/(.*?)=(.*)$/)) {
+          let cookieSplit = cookie.match(/(.*?)=(.*)$/);
+          cookies[cookieSplit[1].trim()] = (cookieSplit[2] || "").trim();
+        }
+        else {
+          cookies = {};
+        }
+      });
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
+    if (cookies["hello"] === "world") {
+      res.write("Yes");
+    } else {
+      res.write("No");
+    }
+    res.end();
+  }
+
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('<h1>404 - page not found</h1>');
+    res.end();
+  }
 });
 
 server.listen(port, () => {
